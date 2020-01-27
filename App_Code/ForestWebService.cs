0adfec04e83e00203e263b11c091d63f630e7d62
Smith <b7040746@hallam.shu.ac.uk>
2020-01-27 19:19:10 +0000

    utilizing the Service class library inside of Forest

---
+++ App_Code/ForestWebService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Forest.Services.IService;
+using Forest.Data.BEANS;
+
+/// <summary>
+/// Summary description for ForestWebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class ForestWebService : System.Web.Services.WebService
+{
+    private Forest.Services.IService.IMusicService _musicService;
+
+    public ForestWebService()
+    {
+        _musicService = new Forest.Services.Service.MusicService();        
+    }
+
+    [WebMethod]
+    public List<MusicBEAN> GetMusicRecordings(int genre)
+    {
+        return _musicService.GetMusicRecordings(genre).ToList();
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+
+}
