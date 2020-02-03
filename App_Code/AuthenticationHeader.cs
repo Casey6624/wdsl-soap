52bd2a658230ef19e2e0cce849332d81b939827c
Smith <b7040746@hallam.shu.ac.uk>
2020-02-03 19:57:24 +0000

    New Leipzig Service implemented

---
+++ App_Code/AuthenticationHeader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for AuthenticationHeader
+/// </summary>
+    
+public class AuthenticationHeader : System.Web.Services.Protocols.SoapHeader
+{
+
+    public string username;
+    public string password;
+
+    
+    public AuthenticationHeader()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
--- App_Code/ForestWebService.cs
+++ App_Code/ForestWebService.cs
@@ -6,15 +6,14 @@ using System.Web.Services;
 using Forest.Services.IService;
 using Forest.Data.BEANS;
 
-/// <summary>
-/// Summary description for ForestWebService
-/// </summary>
 [WebService(Namespace = "http://tempuri.org/")]
 [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
-// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-// [System.Web.Script.Services.ScriptService]
+
+// http://webteach_net.hallam.shu.ac.uk/b7040746/ForestWebService.asmx
 public class ForestWebService : System.Web.Services.WebService
 {
+    public AuthenticationHeader _authenticationHeader;
+
     private Forest.Services.IService.IMusicService _musicService;
 
     public ForestWebService()
@@ -22,10 +21,26 @@ public class ForestWebService : System.Web.Services.WebService
         _musicService = new Forest.Services.Service.MusicService();        
     }
 
-    [WebMethod]
+    [WebMethod, System.Web.Services.Protocols.SoapHeader("_authenticationHeader")]
     public List<MusicBEAN> GetMusicRecordings(int genre)
     {
-        return _musicService.GetMusicRecordings(genre).ToList();
+        //return _musicService.GetMusicRecordings(genre).ToList();
+        if (_authenticationHeader.username == "test" & _authenticationHeader.password == "pass")
+        {
+            try
+            {      
+            return _musicService.GetMusicRecordings(genre).ToList();
+            }
+            catch
+            {
+                throw new Exception("Errorr communicating with web service!");
+            }
+        }
+        else
+        {
+            throw new Exception("Failed to authenticate with web service");
+        }
+
     }
 
     [WebMethod]
